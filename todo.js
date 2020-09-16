1497bc0f542271445f9996a159b3404c0bd7bdd9
Waqas Khattak <waqasktk81@gmail.com>
2020-09-16 10:50:03 +0500

    Storing todos in localStorage

--- todo.js
+++ todo.js
@@ -4,19 +4,38 @@
 // h.appendChild(text)
 // document.querySelector('body').appendChild(h)
 
-
 var ul=document.getElementById('list')
 var li;
+let length=localStorage.length
+for(let i=0;i<length;i++){
+    var item=localStorage.key(i)
+    var textnode=document.createTextNode(item)
+    var checkbox=document.createElement('input')
+        checkbox.setAttribute('type','checkbox')
+        var lable=document.createElement('label')
+        lable.appendChild(textnode)
+        li=document.createElement('li')
+        li.appendChild(checkbox)
+        li.appendChild(lable)
+        // ul.appendChild(li)//this will append it at the end
+        ul.insertBefore(li,ul.childNodes[0])
+
+    
+}
+
+
 
 function addItem(){
     var input=document.getElementById('item')
     var item=input.value;
     var textnode=document.createTextNode(item)
 
+
     if(item ==''){
         alert('Add something')
         return false;
     }else{
+        localStorage.setItem(`${item}`,item)
         var checkbox=document.createElement('input')
         checkbox.setAttribute('type','checkbox')
         var lable=document.createElement('label')
@@ -42,8 +61,10 @@ function removeItem(){
     }else{
         for(var i=0;i<li.length;i++){
         while(li[i] && li[i].children[0].checked){
+            let text=li[i].children[1].innerHTML
+            localStorage.removeItem(text)
             ul.removeChild(li[i])
-                    check++;
+            check++;
 
 
         }
@@ -54,7 +75,7 @@ function removeItem(){
             }else{
                 setTimeout(()=>{
                 alert('Your selected todos has been deleted')
-r                },500)
+                            },500)
             }
 
     }
